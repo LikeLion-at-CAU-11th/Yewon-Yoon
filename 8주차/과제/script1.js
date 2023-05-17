d1e9f3100928161210897f28083db4282486f5ba
kiiiiv <lizzy2@naver.com>
2023-05-18 00:15:29 +0900

    ..

--- 8주차/과제/script1.js
+++ 8주차/과제/script1.js
@@ -1,8 +1,41 @@
 //1.방명록 작성기능
 //2. 방명록 리스트 기능
 //3. 방명록 삭제 기능
+const form=document.getElementById('namelist');
+
 const container=document.getElementById('container');
 const num=document.getElementById('num');
+const writerInput = document.getElementById('writer');
+const titleInput = document.getElementById('title');
+const contentInput = document.getElementById('content');
+writerInput.addEventListener('input', function(event) {
+  writerInput.value = event.target.value;
+});
+titleInput.addEventListener('input', function(event) {
+  titleInput.value = event.target.value;
+});
+contentInput.addEventListener('input', function(event) {
+  contentInput.value = event.target.value;
+});
+//새 방명록 추가하기
+form.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  let formdata = new FormData(form);
+  let formDataJson = {};
+  for (let [key, value] of formdata.entries()) {
+    formDataJson[key] = value;
+  }
+  fetch("http://likelion-toy.kro.kr:8000/posts/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formDataJson),
+  })
+  .then(res => console.log(res.json()))
+  .then(data=>console.log(data));
+});
+
 //모든 방명록 가져오기
 async function getAllData(){
   // await fetch(API요청을 받는 백엔드 주소), {API요청에 담을 정보}
@@ -21,30 +54,16 @@ async function getAllData(){
       return response.status;
   }
 }
-//새 방명록 추가하기
-async function postData(){
-  let fetchData=await fetch("http://likelion-toy.kro.kr:8000/posts/", {
-    method: "POST",
-    mode: 'no-cors',
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      author:"이기웅",
-      title: "방명록1",
-      content: "멋사 파이팅!"
-    }),
-  }).then((response) => console.log(response));
-}
+
 //특정 방명록 가져오기
 async function getData(){
-  let getData=await fetch(("https://jsonplaceholder.typicode.com/posts/3/"))
+  let getData=await fetch(("http://likelion-toy.kro.kr:8000/posts/1/"))
   .then(res =>res.json())
   .then(json =>console.log(json));
 }
 //특정 방명록 삭제하기
 async function deleteData(){
-  let deleteData=await fetch("https://jsonplaceholder.typicode.com/posts/3/", {
+  let deleteData=await fetch("http://likelion-toy.kro.kr:8000/posts/1/", {
     method: "DELETE",
     }).then((response) => console.log(response));
 }

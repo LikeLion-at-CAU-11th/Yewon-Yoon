bff0048bd8624ddde20157f5d2dd126e9efa5253
kiiiiv <lizzy2@naver.com>
2023-07-12 03:14:09 +0900

    [FEAT] refresh 토큰 만료시 로그아웃 처리

--- 12주차/log/src/apis/authAxios.js
+++ 12주차/log/src/apis/authAxios.js
@@ -12,12 +12,20 @@ export const getAuthAxios=(tokens)=>{
         (res)=>res,
         async (error)=>{
             if(error.response.status === 401){
-                const {accessToken, refreshToken} = await getNewRefreshToken(access, refresh);
-                localStorage.setItem('accessToken', accessToken);
-                localStorage.setItem('refreshToken', refreshToken);
-                const config = error.response.config;
-                config.header.Authorization = accessToken;
-                return await axios.get(config.url, config);
+                try{
+                    const {accessToken, refreshToken} = await getNewRefreshToken(access, refresh);
+                    localStorage.setItem('accessToken', accessToken);
+                    localStorage.setItem('refreshToken', refreshToken);
+                    const config = error.response.config;
+                    config.header.Authorization = accessToken;
+                    return await axios.get(config.url, config);
+                }catch(err){
+                    //토큰 정보를 모두 삭제하면 로그아웃 됨
+                    localStorage.removeItem('accessToken');
+                    localStorage.removeItem('refreshToken');
+
+                }
+                
             }
         }
         );

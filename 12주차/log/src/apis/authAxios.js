88da869c44bbb2f7f91c300f9742b8a56f2d689b
kiiiiv <lizzy2@naver.com>
2023-07-12 03:28:11 +0900

    [REFACTOR] refresh 토큰 만료시 로그아웃 처리

--- 12주차/log/src/apis/authAxios.js
+++ 12주차/log/src/apis/authAxios.js
@@ -1,33 +1,26 @@
-import axios from "axios";
-import { getNewRefreshToken } from "./refresh";
-export const getAuthAxios=(tokens)=>{
-    const {access, refresh} = tokens;
-    const authAxios=axios.create({
-        baseURL: 'http://front.cau-likelion.org/',
-        headers:{
-            Authorization: access,
-        },
-    });
-    authAxios.interceptors.response.use(
-        (res)=>res,
-        async (error)=>{
-            if(error.response.status === 401){
-                try{
-                    const {accessToken, refreshToken} = await getNewRefreshToken(access, refresh);
-                    localStorage.setItem('accessToken', accessToken);
-                    localStorage.setItem('refreshToken', refreshToken);
-                    const config = error.response.config;
-                    config.header.Authorization = accessToken;
-                    return await axios.get(config.url, config);
-                }catch(err){
-                    //토큰 정보를 모두 삭제하면 로그아웃 됨
-                    localStorage.removeItem('accessToken');
-                    localStorage.removeItem('refreshToken');
+import axios from 'axios';
+import { getNewRefreshToken } from './refresh';
 
-                }
-                
-            }
-        }
-        );
-    return authAxios;
+export const getAuthAxios = (tokens) => {
+  const { access, refresh } = tokens;
+  const authAxios = axios.create({
+    baseURL: 'http://front.cau-likelion.org/',
+    headers: {
+      Authorization: access,
+    },
+  });
+  authAxios.interceptors.response.use(
+    (res) => res,
+    async (error) => {
+      if (error.response.status === 401) {
+        const { accessToken, refreshToken } = await getNewRefreshToken(access, refresh);
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
+        const config = error.response.config;
+        config.headers.Authorization = accessToken;
+        return await axios.get(config.url, config);
+      }
+    }
+  );
+  return authAxios;
 };
--- 12주차/log/src/pages/Mypage.js
+++ 12주차/log/src/pages/Mypage.js
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { getMyPage } from '../apis/mypage';
+import { useNavigate } from 'react-router-dom';
 
 const Mypage = () => {
     const [data, setData] = useState({});
     const [loading, setLoading] = useState(true);
+    const router = useNavigate
     useEffect(()=>{
         const accessToken = localStorage.getItem('accessToken');
         const refreshToken = localStorage.getItem('refreshToken');
-        getMyPage({access: accessToken, refresh: refreshToken}).then((res)=>{
-            setData(res.data);
-            setLoading(false);
-        });
+        getMyPage({access: accessToken, refresh: refreshToken})
+            .then((res)=>{
+                setData(res.data);
+                setLoading(false);
+            })
+            .catch((error)=>{
+                if (error.response.status === 401){
+                    localStorage.removeItem("accessToken");
+                    localStorage.removeItem("refreshToken");
+                    router("/");
+
+
+                }
+            });
     },[]);
     if (loading) return <div>로딩중..</div>    
     return(
